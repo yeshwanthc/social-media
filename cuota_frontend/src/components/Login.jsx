c876c679c6a14625b2fb4ee2bf236a6286c449f3
yeshwanthc <yeshwanthnaidu62@gmail.com>
2022-07-03 21:45:11 +0530

    5th commit

---
+++ cuota_frontend/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logo2.png'
+
+const Login = () => {
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+      <video 
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoplay
+            className='w-full h-full object-cover'
+        />
+          <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+            <div className='p-2'>
+             <img src={logo} width='130px'/>
+            </div>
+            <div className='shadow-2xl'>
+              <GoogleLogin
+              clientId=''
+              render={(renderProps) => (
+                <button
+                type='button'
+                className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                onClick={renderProps.onClick}
+                
+                >
+                 <FcGoogle className='mr-4'/>Sign in with Google
+                </button>
+
+              )}
+               />
+               
+            </div>
+          </div>  
+         
+      </div>
+    </div>
+  )
+}
+
+export default Login;
